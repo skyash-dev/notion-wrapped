7ba2e7f5efe8bdd26a07135d419ed74e4c43fd57
Yash <yashpc09@gmail.com>
2024-12-27 01:03:31 +0530

    postho fix

--- app/providers.tsx
+++ app/providers.tsx
@@ -15,7 +15,7 @@ type AuthState = {
 const AuthContext = createContext<AuthState | undefined>(undefined);
 
 if (typeof window !== "undefined") {
-  posthog.init("phc_2On2F6GeC9To1qiiBZDZSwZB9L0IDlGXNsqVkgxZhOU", {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
